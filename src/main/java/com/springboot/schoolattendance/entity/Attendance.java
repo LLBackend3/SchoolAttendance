3c7140e3c9ebc4e9c889ed0c4bad0ef860432ebd
wlsdydlee <eagie0529@naver.com>
2025-05-07 18:30:36 +0900

    commit

--- src/main/java/com/springboot/schoolattendance/entity/Attendance.java
+++ src/main/java/com/springboot/schoolattendance/entity/Attendance.java
@@ -1,4 +1,28 @@
 package com.springboot.schoolattendance.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    private boolean present;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
 }
