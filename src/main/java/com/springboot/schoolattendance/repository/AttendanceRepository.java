f0e54c04bd5e33b8fa607d06ac4edb7f40b08bb8
wlsdyd <154351391+wlsdydlee@users.noreply.github.com>
2025-05-08 18:42:34 +0900

    Update AttendanceRepository.java

--- src/main/java/com/springboot/schoolattendance/repository/AttendanceRepository.java
+++ src/main/java/com/springboot/schoolattendance/repository/AttendanceRepository.java
@@ -1,2 +1,8 @@
 package com.springboot.schoolattendance.repository;
 
+import com.springboot.schoolattendance.entity.Classname;
+import com.springboot.schoolattendance.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
